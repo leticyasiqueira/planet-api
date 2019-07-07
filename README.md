427c8985017242450652422cddb51d958dff63a7
Leticya Siqueira <leticya.siqueira@gmail.com>
2019-07-07 10:42:53 -0300

    Cria estrutura inicial
    Cria estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# planet-api
-Micro serviço referente aos panetas do Star War
+# planets-api
+## Tecnologias Utilizadas no Projeto
+* Linguagem de programação: Java
+* Framework: Spring boot
+* Banco de dados: H2 embedded
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student (id,name,email,password, age) values
+    (1, 'leticya','leticya.siqueira@gmail.com', '81934683','26'),
+    (2, 'gustavo','gustavo@gmail.com', '81934683','25'),
+    (3, 'andre','andre@gmail.com', '81934683', '31'),
+    (4, 'anderson','anderson@gmail.com', '81934683', '40'); 
